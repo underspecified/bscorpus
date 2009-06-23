c8afae137af51cac43a5cece869e1d2dbd339bb3
eric-n <eric-n@is.naist.jp>
2009-06-23 16:51:56 +0900

    gr-links.py: collect and count links and reverse links in blog posts

--- bin/gr-links.py
+++ bin/gr-links.py
@@ -4,15 +4,43 @@ import feedparser
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 import urllib2
 import re
+import os
+import sys
 
 links = {}
-d = feedparser.parse(r'/Users/eric/bscorpus/data/test/gr-test.xml')
-for e in d.entries[:1]:
-	print e.link
-	p = urllib2.urlopen(e.link)
-#	a = SoupStrainer('a')
-#	s = BeautifulSoup(p, parseOnlyThese=a)
-	s = BeautifulSoup(p)
-	print s.prettify()
-	for a in s.findAll('a'):
-		print a['href']
+bshome = '%s/bscorpus' % os.environ['HOME']
+x = '%s/data/test/gr-test.xml' % bshome
+#x = '%s/data/gr/gr-feeds.00.xml' % bshome
+d = feedparser.parse(x)
+for e in d.entries:
+#	print >>sys.stderr, e.link
+	try:
+		p = urllib2.urlopen(e.link)
+#		s = BeautifulSoup(p)
+		t = SoupStrainer('a', href=re.compile('http'))
+		s = BeautifulSoup(p, parseOnlyThese=t)
+#		print >>sys.stderr, s.prettify()
+		links.setdefault(e.link, [])
+		for a in s.findAll('a'):
+			links[e.link].append(a['href'])
+#			print >>sys.stderr, a['href']
+	except:
+		print >>sys.stderr, "FAIL! >_<", e.link
+
+rlinks = {}
+for l in sorted(links):
+	for a in sorted(links[l]):
+		rlinks.setdefault(a, [])
+		rlinks[a].append(l)
+#		print >>sys.stderr, l, a
+
+#for a in sorted(rlinks.keys(), key=lambda x: len(rlinks[x]), reverse=True):
+#	print len(rlinks[a]), a
+#	for l in sorted(rlinks[a]):
+#		print >>sys.stderr, a, l
+
+for l in sorted(links):
+	if l in rlinks:
+		for a in sorted(rlinks[l]):
+			if a != l:
+				print a, l
