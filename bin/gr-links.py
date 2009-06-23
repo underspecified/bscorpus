8dee6725b5b7938191d2da27556e56dc0da6a30a
eric-n <eric-n@is.naist.jp>
2009-06-23 21:32:32 +0900

    gr-links.py: cleaned up code, added utf-8 i/o support, get XML files from command line. Makefile: added recipe for gr-links.txt

--- bin/gr-links.py
+++ bin/gr-links.py
@@ -1,46 +1,68 @@
 #!/usr/bin/python2.5
+# -*- coding: utf-8 -*-
 
 import feedparser
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 import urllib2
 import re
 import os
+
+# utf-8 i/o plz!
 import sys
+import codecs 
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+sys.stdin = codecs.getwriter('utf-8')(sys.stdin)
 
 links = {}
-bshome = '%s/bscorpus' % os.environ['HOME']
-x = '%s/data/test/gr-test.xml' % bshome
-#x = '%s/data/gr/gr-feeds.00.xml' % bshome
-d = feedparser.parse(x)
-for e in d.entries:
-#	print >>sys.stderr, e.link
-	try:
-		p = urllib2.urlopen(e.link)
-#		s = BeautifulSoup(p)
-		t = SoupStrainer('a', href=re.compile('http'))
-		s = BeautifulSoup(p, parseOnlyThese=t)
-#		print >>sys.stderr, s.prettify()
-		links.setdefault(e.link, [])
-		for a in s.findAll('a'):
-			links[e.link].append(a['href'])
-#			print >>sys.stderr, a['href']
-	except:
-		print >>sys.stderr, "FAIL! >_<", e.link
-
 rlinks = {}
-for l in sorted(links):
-	for a in sorted(links[l]):
-		rlinks.setdefault(a, [])
-		rlinks[a].append(l)
-#		print >>sys.stderr, l, a
 
-#for a in sorted(rlinks.keys(), key=lambda x: len(rlinks[x]), reverse=True):
-#	print len(rlinks[a]), a
-#	for l in sorted(rlinks[a]):
-#		print >>sys.stderr, a, l
+t = SoupStrainer('a', href=re.compile('http'))
+def get_links(x):
+	'''Retrieve links from blog posts in XML file.'''
+	d = feedparser.parse(x)
+	for e in d.entries:
+		try:
+			l = e.link
+			p = urllib2.urlopen(l)
+			links.setdefault(l, [])
+	
+			s = BeautifulSoup(p, parseOnlyThese=t)
+#			print >>sys.stderr, s.prettify()
+			for a in s.findAll('a'):
+				h = a['href']
+				links[l].append(h)
+				rlinks.setdefault(h, [])
+				rlinks[h].append(l)
+#				print >>sys.stderr, h
+	
+			print >>sys.stderr, "WIN! \(^o^)/", e.link
+	
+		except Exception, err:
+			print >>sys.stderr, "FAIL! >_<", err, e.link
 
-for l in sorted(links):
-	if l in rlinks:
-		for a in sorted(rlinks[l]):
-			if a != l:
-				print a, l
+def print_links():
+	'''Print links with counts.'''
+	for a in sorted(rlinks.keys(), key=lambda x: len(rlinks[x]), reverse=True):
+		print len(rlinks[a]), a
+#		for l in sorted(rlinks[a]):
+#			print >>sys.stderr, a, l
+
+def print_rlinks():
+	'''Print reverse links if they aren't self-refererential.'''
+	for l in sorted(links):
+		if l in rlinks:
+			for a in sorted(rlinks[l]):
+				if a != l:
+					print a, l
+
+#bshome = '%s/bscorpus' % os.environ['HOME']
+#x = '%s/data/test/gr-test.xml' % bshome
+#x = '%s/data/gr/gr-feeds.00.xml' % bshome
+
+if len(sys.argv) > 1:
+	for x in sys.argv[1:]:
+		get_links(x)
+	print_links()
+	print_rlinks()
+else:
+	print >>sys.stderr, 'usage: pr-links.py <google-reader.xml> [<google-reader.xml> ...]'
