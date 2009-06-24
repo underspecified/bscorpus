8ebd9f8b7188cc77f80b1b3e77d97cc3a22f2dee
eric-n <eric-n@is.naist.jp>
2009-06-24 15:00:41 +0900

    gr-links.py: print links from blog summaries, the blog posts linking to them, and a count of linked posts.

--- bin/gr-links.py
+++ bin/gr-links.py
@@ -15,6 +15,7 @@ sys.stdin = codecs.getwriter('utf-8')(sys.stdin)
 
 links = {}
 rlinks = {}
+blogs = {}
 
 t = SoupStrainer('a', href=re.compile('http'))
 def get_links(x):
@@ -23,46 +24,59 @@ def get_links(x):
 	for e in d.entries:
 		try:
 			l = e.link
-			p = urllib2.urlopen(l)
-			links.setdefault(l, [])
-	
-			s = BeautifulSoup(p, parseOnlyThese=t)
-#			print >>sys.stderr, s.prettify()
-			for a in s.findAll('a'):
-				h = a['href']
-				links[l].append(h)
-				rlinks.setdefault(h, [])
-				rlinks[h].append(l)
-#				print >>sys.stderr, h
-	
-			print >>sys.stderr, "WIN! \(^o^)/", e.link
+			b = e.source.link
+			
+			try:
+				blogs.setdefault(l, set())
+				blogs[l].add(b)
+				links.setdefault(l, [])
+
+				p = None
+				if 'summary' in e:
+					p = e.summary
+				elif 'subtitle' in e:
+					p = e.subtitle
+				elif 'content' in e and 'value' in e.content:
+					p = e.content.value
+#				else:
+#					p = urllib2.urlopen(l)
+				s = BeautifulSoup(p, parseOnlyThese=t)
+#				print >>sys.stderr, s.prettify()
+
+				for a in s.findAll('a'):
+					h = a['href']
+					links[l].append(h)
+					rlinks.setdefault(h, [])
+					rlinks[h].append(l)
+					blogs.setdefault(h, set())
+					blogs[h].add(b)
+#					print >>sys.stderr, h
+
+				print >>sys.stderr, "WIN! \(^o^)/", l
+
+			except Exception, err:
+				print >>sys.stderr, "FAIL! >_<", err, l
 	
 		except Exception, err:
-			print >>sys.stderr, "FAIL! >_<", err, e.link
+			print >>sys.stderr, "EPIC FAIL! Orz", err, e.id
 
-def print_links():
-	'''Print links with counts.'''
-	for a in sorted(rlinks.keys(), key=lambda x: len(rlinks[x]), reverse=True):
-		print len(rlinks[a]), a
-#		for l in sorted(rlinks[a]):
-#			print >>sys.stderr, a, l
+
+def fmt_rlinks():
+	'''Throw away link spam and self-referential links.'''
+	for l in rlinks:
+		rlinks[l] = set([x for x in rlinks[l] if blogs[x]!=blogs[l] and x!=l])
 
 def print_rlinks():
 	'''Print reverse links if they aren't self-refererential.'''
-	for l in sorted(links):
-		if l in rlinks:
-			for a in sorted(rlinks[l]):
-				if a != l:
-					print a, l
-
-#bshome = '%s/bscorpus' % os.environ['HOME']
-#x = '%s/data/test/gr-test.xml' % bshome
-#x = '%s/data/gr/gr-feeds.00.xml' % bshome
+	for l in sorted(set(rlinks), key=lambda x: len(rlinks[x]), reverse=True):
+		d = rlinks[l]
+		if len(d) > 0:
+			print >>sys.stdout, len(d), l, d
 
 if len(sys.argv) > 1:
 	for x in sys.argv[1:]:
 		get_links(x)
-	print_links()
+	fmt_rlinks()
 	print_rlinks()
 else:
 	print >>sys.stderr, 'usage: pr-links.py <google-reader.xml> [<google-reader.xml> ...]'
