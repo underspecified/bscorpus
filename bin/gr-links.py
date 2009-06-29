8d52a6f628a694d684ea99ededdd9c2156ac7ad5
eric-n <eric-n@is.naist.jp>
2009-06-29 10:35:27 +0900

    gr-links.py: retrieve titles of blog posts and links in posts.

--- bin/gr-links.py
+++ bin/gr-links.py
@@ -12,40 +12,48 @@ import codecs
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stdin = codecs.getwriter('utf-8')(sys.stdin)
 
+titles = SoupStrainer('title')
+def get_title(h):
+	'''Retrieve title of a web page.'''
+	try:
+		s = BeautifulSoup(urllib2.urlopen(h), parseOnlyThese=titles)
+		return s.title.string
+	except Exception, err:
+		return ''
+
+blog = {}
 links = {}
 rlinks = {}
-blog = {}
 tags = {}
 rtags = {}
 title = {}
 
+google_tags = set([u'fresh', u'read', u'reading-list'])
 anchors = SoupStrainer('a', href=re.compile('http'))
+
 def get_links(x):
 	'''Retrieve links from blog posts in XML file.'''
 	d = feedparser.parse(x)
 	for e in d.entries:
 		try:
-			l = e.link # link to blog post
 			b = e.source.link # blog source
-			i = e.title # blog post title
+			i = e.title       # blog post title
+			l = e.link        # link to blog post
 
 			# try to get permalink by following redirects
 			try:
-				if b not in l:
-					l = urllib2.urlopen(l).geturl()
+				if b not in l: l = urllib2.urlopen(l).geturl()
 			except Exception, err:
 				pass
-	
+
 			try:	
 				blog[l] = b
-				title[l] = i
-				tags[l] = set([t.label or t.term for t in e.tags if t.label or t.term]) 
-				tags[l] -= set([u'fresh', u'reading-list'])
+				tags[l] = set([t.label or t.term for t in e.tags if t.label or t.term]) - google_tags
 #				print >>sys.stderr, tags[l]
 				for t in tags[l]:
 					rtags.setdefault(t, set())
 					rtags[t].add(l)
-				links.setdefault(l, [])
+				title[l] = i
 
 				# get blog post summary by trying several RSS aliases
 				p = None
@@ -62,13 +70,16 @@ def get_links(x):
 				s = BeautifulSoup(p, parseOnlyThese=anchors)
 #				print >>sys.stderr, s.prettify()
 
+				# index links in blog post summary
+				links.setdefault(l, [])
 				for a in s.findAll('a'):
 					h = a['href']
+					blog.setdefault(h, '')
 					links[l].append(h)
 					rlinks.setdefault(h, [])
 					rlinks[h].append(l)
-					blog.setdefault(h, '')
 					tags.setdefault(h, set())
+#					title.setdefault(h, get_title(h))
 #					print >>sys.stderr, h
 
 				print >>sys.stderr, "WIN! \(^o^)/", l
@@ -87,12 +98,13 @@ def fmt_rlinks():
 
 def print_rlinks():
 	'''Print reverse links if they aren't self-refererential.'''
-	for l in sorted(rlinks, key=lambda x: len(rlinks[x]), reverse=True):
+	for l in sorted([l for l in rlinks if l not in blog.values()],
+					key=lambda x: len(rlinks[x]), reverse=True):
 		d = rlinks[l]
 		i = [title[i] for i in d if i in title]
 		t = sorted(reduce(set.union, [tags[l]]+[tags[z] for z in d if z in tags]))
 		if len(d) > 1:
-			print >>sys.stdout, len(d), l, d, i, t
+			print >>sys.stdout, len(d), get_title(l), l, i, t, d
 
 if len(sys.argv) > 1:
 	for x in sys.argv[1:]:
