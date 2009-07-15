dc75e038d1fb49fbdf7a808393f9ce9a3d4fd388
eric-n <eric-n@is.naist.jp>
2009-07-15 18:10:13 +0900

    pyutils.py: improved progress bar for filter_rlinks().

--- bin/grutils.py
+++ bin/grutils.py
@@ -134,9 +134,10 @@ def has_many_blogs(_links):
 
 def filter_rlinks():
 	'''Throw away link spam and self-referential links.'''
-	print >>sys.stderr, "Filtering rlinks ...",
+	print >>sys.stderr, "Filtering rlinks . . .",
 	clinks = {}
-	for l in rlinks:
+	for i,l in enumerate(rlinks):
+		if (i % 10) == 0: print >>sys.stderr, '.',
 		# only keep non-self-referential reverse links that are from different blogs
 		c = rlinks[l]
 #		print >>sys.stderr, has_many_blogs(c), c
