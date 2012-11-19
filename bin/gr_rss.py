ed10904740d3a7e1eacaaa3fd8ca43aaa14a89ee
underspecified <underspecified@gmail.com>
2012-11-19 19:03:08 +0900

    gr_rss.py: code cleanup

--- bin/gr_rss.py
+++ bin/gr_rss.py
@@ -28,11 +28,13 @@ def gr_auth(user, passwd):
         #print >>sys.stderr, 'auth:', auth
         return {'Authorization': 'GoogleLogin auth=%s' % auth['Auth']}
 
+cont_re = re.compile(r'<gr:continuation>(.+)</gr:continuation>')
 def get_cont(content):
-	m = re.search(r'<gr:continuation>(.+)</gr:continuation>',content)
+	m = cont_re.search(content)
 	if m:
 		return m.group(1)
-	return None
+        else:
+                return None
 
 def get_feeds(base_url, header):
         cont_url = base_url
@@ -44,26 +46,26 @@ def get_feeds(base_url, header):
                         yield content
                         cont = get_cont(content)
                         if not cont: break
-                        cont_url = "%s&c=%s" % (base_url, cont)
-                        #print >>sys.stderr, "cont_url: %s" % cont_url
+                        cont_url = '%s&c=%s' % (base_url, cont)
+                        #print >>sys.stderr, 'cont_url: %s' % cont_url
                 except Exception as err:
                         print >>sys.stderr, 'Error getting feeds! >_<', err
                         raise err
 
 def main():
-        fs = "gr-feeds"
+        fs = 'gr_feeds'
         posts = 1000
         if (len(sys.argv) >= 1):
                 fs = sys.argv[1]
         if (len(sys.argv) > 2):
                 posts = int(sys.argv[2])
-        fmt = "%%s.%%0%dd.xml" % 3
+        fmt = '%%s.%%0%dd.xml' % 3
         header = gr_auth('genronmappu@gmail.com', 'matsumoto')
         #print >>sys.stderr, 'header:', header
         base_url = 'https://www.google.com/reader/atom/?n=%d' % posts
         for i, feed in enumerate(get_feeds(base_url, header)):
                 file = fmt % (fs, i)
-                print >>sys.stderr, "Making file %s ..." % file
+                print >>sys.stderr, 'Making file %s ...' % file
                 with open(file, 'w') as fh:
                         print >>fh, feed
 
