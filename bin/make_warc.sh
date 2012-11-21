7b9b1f01854f33dd96ec45cfeca6264f22133d45
underspecified <underspecified@gmail.com>
2012-11-21 15:23:42 +0900

    make_warc.sh: bug fixes for linux bash

--- bin/make_warc.sh
+++ bin/make_warc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cmd="wget -nv -T60 --no-warc-compression --warc-max-size=1G --warc-cdx \
-     --warc-file=$1 -i $2 -O- >/dev/null"
+warc=$1
+urls=$2
+
+cmd="( wget -nv -T60 --no-warc-compression --warc-max-size=1G --warc-cdx \
+     -O- --warc-file=$warc -i $urls ) >/dev/null"
 echo $cmd
-$cmd
+eval $cmd
